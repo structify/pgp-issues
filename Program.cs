049636bdd031b790f47747e6b678986b6dd48b96
Jason DeVine <jdevine@unifiedlogic.com>
2017-05-23 11:12:10 -0400

    pgp encryption issue

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using DidiSoft.Pgp;
+
+namespace PGPEncryption
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var pgpLib = new PGPLib();
+            Console.WriteLine("successfully created");
+        }
+    }
+}
